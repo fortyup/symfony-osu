583d02f689ef3298cbec55831037cef274c0b3c2
CAPEL Maxime <maxime.capel19@gmail.com>
2022-10-25 10:39:18 +0200

    add user & contact when success

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,18 +4,30 @@ namespace App\Controller;
 
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $form = $this->createForm(ContactType::class);
-        return $this->renderForm('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-            'formulaire' => $form,
-        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $email = $data['email'];
+
+            return $this->render('contact/success.html.twig', [
+                'email' => $email
+            ]);
+        } else {
+            return $this->renderForm('contact/index.html.twig', [
+                'controller_name' => 'ContactController',
+                'formulaire' => $form,
+            ]);
+        }
     }
 }
