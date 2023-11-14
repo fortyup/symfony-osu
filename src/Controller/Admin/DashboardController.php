93064761beba3f7697fc806c11bb14eedc84c4fa
CAPEL Maxime <83071634+fortyup@users.noreply.github.com>
2023-11-14 11:36:14 +0100

    update dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,49 +5,46 @@ namespace App\Controller\Admin;
 use App\Entity\Article;
 use App\Entity\Contact;
 use App\Entity\Users;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+    private $entityManager; // Declare the EntityManager
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager; // Inject the EntityManager
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        // return parent::index();
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        return $this->redirect($adminUrlGenerator->setController(UsersCrudController::class)->generateUrl());
+        $usersCount = $this->entityManager->getRepository(Users::class)->count([]);
+        $articlesCount = $this->entityManager->getRepository(Article::class)->count([]);
+        $contactsCount = $this->entityManager->getRepository(Contact::class)->count([]);
 
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        //return $this->render('some/path/my-dashboard.html.twig');
-    }
-
-    public function configureDashboard(): Dashboard
-    {
-        return Dashboard::new()
-            ->setTitle('App');
+        return $this->render('admin/dashboard.html.twig', [
+            'usersCount' => $usersCount,
+            'articlesCount' => $articlesCount,
+            'contactsCount' => $contactsCount,
+        ]);
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToUrl('Retour au site', 'fa-solid fa-clock-rotate-left', '/');
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Users', 'fa-solid fa-user', Users::class);
-        yield MenuItem::linkToCrud('Article', 'fa-solid fa-newspaper', Article::class);
-        yield MenuItem::linkToCrud('Contact', 'fa-solid fa-envelope', Contact::class);
+        return [
+            MenuItem::linkToUrl('Back to website', 'fa-solid fa-clock-rotate-left', '/'),
+            MenuItem::section('Dashboard'),
+            MenuItem::linkToCrud('Users', 'fa-solid fa-user', Users::class),
+            MenuItem::linkToCrud('Articles', 'fa-solid fa-newspaper', Article::class),
+            MenuItem::linkToCrud('Contacts', 'fa-solid fa-envelope', Contact::class),
+        ];
     }
 }
